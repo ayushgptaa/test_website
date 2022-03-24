7ebf6fbe0c2ef4ffe1be7aa33ac4e8bdca685748
ayushgptaa <ayush.gupta.jk@gmail.com>
2022-03-24 16:10:36 +0530

    Added AvertaStd font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.scss'
+
+import 'styles/globals.scss'
+import 'styles/fonts.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return <Component {...pageProps} />
