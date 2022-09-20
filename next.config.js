30e8238e62aec5a85327906b5dff85752613f880
ayushgptaa <ayush.gupta.jk@gmail.com>
2022-09-20 12:40:48 +0530

    change in next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
